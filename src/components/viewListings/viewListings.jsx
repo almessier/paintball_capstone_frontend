03a3a7667f0c1b939d3149575a456548788d2c9c
Alexander Messier <alexander.j.messier@gmail.com>
2021-11-09 09:15:38 -0600

    Added a google map to ViewListing component.

--- src/components/viewListings/viewListings.jsx
+++ src/components/viewListings/viewListings.jsx
@@ -1,18 +1,49 @@
-import React from "react";
-import "./viewListings"
-import axios from 'axios';
-import useForm from '../../hooks/useForm';
-import { useHistory } from "react-router-dom";
 
+import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
-const ViewListings = (props) => {
+const containerStyle = {
+  width: '1200px',
+  height: '700px'
+};
 
+const center = {
+  lat: 43.132990,
+  lng: -87.905930
+};
 
-    
-    return (
-        <div></div>
-    );
+function ViewListing() {
+  const apiMapsKey = `${process.env.REACT_APP_MAPS_API_KEY}`
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: apiMapsKey
+  })
 
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        defaultCenter={center}
+        center={center}
+        zoom={14}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        
+        <></>
+      </GoogleMap>
+  ) : <></>
 }
 
-export default ViewListings;
+export default React.memo(ViewListing)
