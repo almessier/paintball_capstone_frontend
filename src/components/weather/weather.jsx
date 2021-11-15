6b9d0083d47649b63f64f32d9e4c2a99d4520b30
Alexander Messier <alexander.j.messier@gmail.com>
2021-11-15 05:26:39 -0600

    Styled the weather page.

--- src/components/weather/weather.jsx
+++ src/components/weather/weather.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import { useHistory } from "react-router-dom";
+import moment from 'moment';
 
 function Weather(props) {
 
@@ -9,12 +10,18 @@ function Weather(props) {
         getWeather();
     }, []);
 
+    const dateOptions = {
+        year: "numeric",
+        month:"short",
+        day:"2-digit"
+    }
+
     const history = useHistory();
     const [weather, setWeather] = useState(null);
 
     const getWeather = async () => {
         try{
-            let response = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${props.listedUser.lat},${props.listedUser.lng}&days=7&aqi=no&alerts=no/`);
+            let response = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${props.listedUser.lat},${props.listedUser.lng}&days=3&aqi=no&alerts=no/`);
             setWeather(response.data);
         }
               
@@ -23,43 +30,62 @@ function Weather(props) {
         }
     }
 
-
-    // const getListed = async () => {
-    //     try{
-    //         let response = await axios.get(`http://localhost:8000/api/auth/getalllisted/`)
-    //         let listedUsers = response.data;
-    //         props.setListedUsers(listedUsers);
-    //     }
-              
-    //     catch(ex){
-    //         console.log('Error in getListed API call', ex)
-    //     }
-    // }
-
     return (
-        <div>
-            <div>
-                {props.listing.name}
-                {props.listedUser.username}
-                {props.listedUser.address}
-                {props.listing.start_time}
-                {props.listing.end_time}
-                {props.listing.start_date}
+        <div className='container-fluid'>
+            <div className='row'>
+                <div className='col-2 gutter' />
+                <div className='col-8 list-header'>
+                    <h2>Selected Listing</h2>
+                    <h5>{props.listing.name}</h5>
+                    <div>Username: {props.listedUser.username}</div>
+                    <div>Address: {props.listedUser.address}</div>
+                    <div>Start: {moment(props.listing.start_time.slice(0,5), ['HH:mm']).format('hh:mm A')}</div>
+                    <div>End: {moment(props.listing.end_time.slice(0,5), ['HH:mm']).format('hh:mm A')}</div>
+                    <div>Date: {new Date(props.listing.start_date).toLocaleDateString('en-US', dateOptions)}</div>
+                </div>
+                <div className='col-2 gutter' />
             </div>
-            {weather &&
-            weather.forecast.forecastday.map(day => {
-                return(
-                    <>
-                        <div>
-                            {day.date}
-                            {day.day.maxtemp_f}
-                            {day.day.mintemp_f}
-                            {day.day.avgtemp_f}
-                            {day.day.condition.text}
-                        </div>
-                    </>
-                ) 
-            })}
+            <div className='row'>
+                <div className='col-2 gutter' />
+                    <div className='col-8'>
+                        <br />
+                        <br />
+                        <h2>Three Day Forecast</h2>
+                        <br />
+                        {weather &&
+                        weather.forecast.forecastday.map(day => {
+                            return(
+                                <>
+                                    <div className='container-fluid'>
+                                        <div className='row'>
+                                            <div className='col-4'>
+                                                <br />
+                                                <h5>{new Date(day.date).toLocaleDateString('en-US', dateOptions)}</h5>
+                                            </div>
+                                            <div className='col-2'>
+                                                <div>High:</div>
+                                                <div>Low:</div>
+                                                <div>Average:</div>
+                                            </div>
+                                            <div className='col-4'>
+                                                <div>{day.day.maxtemp_f} °F</div>
+                                                <div>{day.day.mintemp_f} °F</div>
+                                                <div>{day.day.avgtemp_f} °F</div>
+                                            </div>
+                                            <div className='col-2'>
+                                                <div>{day.day.condition.text}</div>
+                                                <img src={day.day.condition.icon} alt={day.day.condition.text} />
+                                            </div>
+                                            <hr />
+                                        </div>
+                                    </div>
+                                </>
+                            ) 
+                        })}
+                    </div>
+                    <div className='col-2 gutter' />
+                </div>
+
         </div>
     )
 }
